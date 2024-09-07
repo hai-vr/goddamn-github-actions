352f8d6920630162765c9236359f9e3720bccd29
Haï~ <hai-vr@users.noreply.github.com>
2024-09-07 03:32:13 +0200

    try add assembly ver

--- goddamn-github-actions/src/VERSION.cs
+++ goddamn-github-actions/src/VERSION.cs
@@ -1,6 +1,16 @@
+using System.Globalization;
+using System.Reflection;
+
 namespace goddamn_github_actions;
 
 internal class VERSION
 {
-    public static string version { get; } = "V?.?.?";
+    // ReSharper disable once InconsistentNaming
+    public static string version { get; private set; }
+
+    static VERSION()
+    {
+        var v = Assembly.GetExecutingAssembly().GetName().Version;
+        version = string.Format(CultureInfo.InvariantCulture, "v{0}.{1}.{2}", v.Major, v.Minor, v.Build);
+    }
 }
